3b9de675ce8a8f46bc82a7a049ad954d835f1203
Rohan <rohanmalaveshwara@gmail.com>
2026-05-04 17:45:04 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print("+ --> Addition")
+print("- --> substraction")
+print(" * -->Multipliction")
+print("/ ---> Division")
+print("% --- Modulus")
+a=int(input("Enter a number :"))
+b=int(input("Enter a number:"))
+
+operator=input("Enter a operator:")
+
+if operator =="+":
+    print(a+b)
+elif operator =="-":
+    print(a-b)
+elif operator =="*":
+    print(a*b)
+elif operator =="/":
+    print(a/b)
+elif operator =="%":
+    print(a/b)
